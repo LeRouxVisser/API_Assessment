d8b97086db301c74cd6100893d896f39e6bd397c
LeRouxVisser <leroux.visser@gmail.com>
2020-09-25 11:59:41 +0200

    Added scalability and error handling to my solution.

--- APIApplication/models.py
+++ APIApplication/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class CoinList(models.Model):
+    Date = models.DateTimeField()
+    jason_str = models.TextField(max_length=34783)
+
+    def __str__(self):
+        return self.jason_str
+
+class MCap(models.Model):
+    Date_captured = models.DateTimeField()
+    coin_id = models.CharField(max_length=50)
+    Input_date = models.DateTimeField()
+    currency = models.CharField(max_length=50)
+    Market_cap = models.FloatField()
+
+    def __float__(self):
+        return self.Market_cap
--- APIApplication/views.py
+++ APIApplication/views.py
@@ -1,42 +1,72 @@
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
 import requests
-from httplib2 import Http
 from datetime import datetime
+from .models import *
+import json
 
 @api_view(["GET"])
 ###########################################################################################################
 ### Used the /coins/list endpoint on https://www.coingecko.com/en/api/v3 url to retrieve                ###
-### a list of coin ids, symboles and names                                                              ###
+### a list of coin ids, symbols and names                                                              ###
 ###########################################################################################################
 def ListCoins(request):
-    h = Http()
     url = "https://api.coingecko.com/api/v3/coins/list"
-    r = requests.get(url)
-    return JsonResponse(r.json() ,safe=False)
+    date_now = datetime.now().date()
+    try:
+        all_events = str(CoinList.objects.filter(Date=date_now)[0])
+    except IndexError:
+        print('Hello')
+        CoinList.objects.all().delete()
+        all_events = requests.get(url).text
+        list_coin_obj = CoinList(Date=date_now, jason_str=all_events)
+        list_coin_obj.save()
+    event_JSON = json.loads(all_events)
+    return JsonResponse(event_JSON, safe=False)
 @api_view(["GET"])
 ###########################################################################################################
-### Used the /market_chart endpoint on https://www.coingecko.com/en/api/v3 url to claculate and         ###
+### Used the /market_chart endpoint on https://www.coingecko.com/en/api/v3 url to calculate and         ###
 ### retrieve the market cap for the wanted coin id and currency pair for the given date.                ###
 ### Made sure that I always get a given days market cap by passing 91 days as a parameter even if the   ###
 ### date passed is not 91 days ago, if however it is more than 91 days ago I will pass the number of    ###
 ### days in the past we would like data for.                                                            ###
 ###########################################################################################################
 def MarketCap(request):
-    h = Http()
     coin_id = request.GET.get('coin_id','')
-    date = datetime.strptime(request.GET.get('date', ''), '%Y/%m/%d').date()
-    currancy = request.GET.get('currency', '')
-    date_now = datetime.now().date()
-    date_diff = (date_now - date).days
-    if date_diff <= 90:
-        days = 91
-    else:
-        days = date_diff + 1
-    url =f'https://api.coingecko.com/api/v3/coins/{coin_id}/market_chart?vs_currency={currancy}&days={days}'
-    r = requests.get(url).json()['market_caps']
+    try:
+        date = datetime.strptime(request.GET.get('date', ''), '%Y/%m/%d').date()
+        currency = request.GET.get('currency', '')
+        date_now = datetime.now().date()
+        date_diff = (date_now - date).days
+        if date_diff <= 90:
+            days = 91
+        else:
+            days = date_diff + 1
+        url = f'https://api.coingecko.com/api/v3/coins/{coin_id}/market_chart?vs_currency={currency}&days={days}'
+
+        try:
+            clear_all = True
+            Date_check = str(MCap.objects.filter(Date_captured=date_now)[0])
+            clear_all = False
+            market_cap = float(
+                MCap.objects.filter(Date_captured=date_now, Input_date=date, coin_id=coin_id, currency=currency)[0])
+        except IndexError:
+            if clear_all == True:
+                MCap.objects.all().delete()
+            try:
+                market_caps = requests.get(url).json()['market_caps']
+                for i in range(len(market_caps) - 1):
+                    if date == datetime.fromtimestamp(market_caps[i][0] / 1000).date():
+                        market_cap = market_caps[i][1]
+                list_mcap_obj = MCap(Date_captured=date_now, Input_date=date, coin_id=coin_id, currency=currency,
+                                     Market_cap=market_cap)
+                list_mcap_obj.save()
+            except KeyError:
+                return JsonResponse(requests.get(url).json(), safe=False)
+    except ValueError:
+        return JsonResponse({'error':'Invalid date please use yyyy/mm/dd format'}, safe=False)
+
     dict_response = {}
-    for i in range(len(r)-1):
-        if date == datetime.fromtimestamp(r[i][0]/1000).date():
-            dict_response[currancy] = r[i][1]
+    dict_response[currency] = market_cap
     return JsonResponse(dict_response, safe=False)
+
